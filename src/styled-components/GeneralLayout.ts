8b22ba6d2b14133812f30b1518d9666bbefd0674
SeroTomas <serocontacto@gmail.com>
2023-12-26 19:12:31 -0300

    added styled components, start with general layout

---
+++ src/styled-components/GeneralLayout.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Primary } from "@/styled-components";
+
+export const GeneralLayout = styled.div`
+    display: grid;
+    place-items: center;
+    background-color: ${Primary.yellow};
+`
---
+++ src/styled-components/colors.ts
@@ -0,0 +1,13 @@
+// Primary colors
+
+export enum Primary {
+    yellow = "hsl(47, 88%, 63%)"
+}
+
+export enum Neutral {
+    white = "hsl(0, 0%, 100%)",
+    grey = "hsl(0, 0%, 50%)",
+    black = "hsl(0, 0%, 7%)"
+}
+
+
