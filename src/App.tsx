127b8f7d195104a87a7801476dc5036c6d9c39fe
SeroTomas <serocontacto@gmail.com>
2023-12-27 01:32:54 -0300

    try to fix svg routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Card, GeneralLayout } from "./components";
 import { CardModel } from "./models";
 
 const cardInfo: CardModel = {
-  image: "https://blog-preview-card-main-pi.vercel.app/public/images/illustration-article.svg",
+  image: "public/images/illustration-article.svg",
   chip: "Learning",
   date: "Published 21 Dec 2023",
   title: "HTML & CSS fundations",
   text: "These languajes are the backcone of every website, defining structure, contents, and presentation.",
   profile: {
-    avatar: "https://blog-preview-card-main-pi.vercel.app/public/images/image-avatar.webp",
+    avatar: "public/images/image-avatar.webp",
     name: "Greg Hooper"
   }
 }
