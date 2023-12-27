a3970ef2bc75ef8dae240983ccb708acc2d7b1d0
SeroTomas <serocontacto@gmail.com>
2023-12-27 01:22:54 -0300

    hover title fix

--- src/components/Title.ts
+++ src/components/Title.ts
@@ -1,6 +1,13 @@
+import { Primary } from "@/utilities";
 import styled from "styled-components";
 
 export const Title = styled.h2`
     font-size: 2rem;
     font-weight: 800;
+
+    &:hover{
+        cursor: pointer;
+        color: ${Primary.yellow};
+    }
+
 `
